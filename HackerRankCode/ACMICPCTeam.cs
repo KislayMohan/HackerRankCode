8fe4e7d403f28a2bdf03dda389e0e2a33ee952f0
KISLAY\Kislay <kislay_live@hotmail.com>
2021-05-04 19:39:35 +0800

    Added OrganizingContainer

---
+++ HackerRankCode/ACMICPCTeam.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCode
+{
+    public class ACMICPCTeam
+    {
+        public static void ACMICPCTeamFlow()
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+            List<string> topic = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string topicItem = Console.ReadLine();
+                topic.Add(topicItem);
+            }
+            List<int> result = AcmTeam(topic);
+            Console.WriteLine(String.Join("\n", result));
+        }
+
+        private static List<int> AcmTeam(List<string> topic)
+        {
+            var result = new List<int>();
+            int maxSubCount = 0, maxTeamCount = 0;
+            for (int i = 0; i < topic.Count - 1; i++)
+            {
+                for (int j =  i + 1; j < topic.Count; j++)
+                {
+
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ HackerRankCode/OrganizingContainer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCode
+{
+    public class OrganizingContainer
+    {
+        public static void OrganizingContainerFlow()
+        {
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+                List<List<int>> container = new List<List<int>>();
+                for (int i = 0; i < n; i++)
+                {
+                    container.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(containerTemp => Convert.ToInt32(containerTemp)).ToList());
+                }
+                string result = OrganizingContainers(container);
+                Console.WriteLine(result);
+            }
+        }
+
+        private static string OrganizingContainers(List<List<int>> container)
+        {
+            var containerCapacityList = new int[container.Count];
+            var ballquantityList = new int[container.Count];
+
+            for (int i = 0; i < container.Count; i++)
+            {
+                for (int j = 0; j < container[i].Count; j++)
+                {
+                    containerCapacityList[i] += container[i][j];
+                    ballquantityList[j] += container[i][j];
+                }
+            }
+
+            Array.Sort(containerCapacityList);
+            Array.Sort(ballquantityList);
+
+            for (int i = 0; i < containerCapacityList.Length; i++)
+            {
+                if (containerCapacityList[i] != ballquantityList[i])
+                {
+                    return "Impossible";
+                }
+            }
+            return "Possible";
+        }
+    }
+}
