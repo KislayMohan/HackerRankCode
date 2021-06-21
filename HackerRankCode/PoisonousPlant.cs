ae08a8e01aa01ef3a470c45d87428dbda244e29c
KISLAY\Kislay <kislay_live@hotmail.com>
2021-06-21 18:16:43 +0800

    Added improved postionous plant solution.

--- HackerRankCode/PoisonousPlant.cs
+++ HackerRankCode/PoisonousPlant.cs
@@ -39,5 +39,62 @@ namespace HackerRankCode
             return days;
 
         }
+
+        public static int ImprovedpoisonousPlants(int[] p)
+        {
+            int days = 0;
+            var LinkList = new LinkedList<List<int>>();
+            var plantList = new List<int>();
+            foreach (var item in p)
+            {
+                if (plantList.Count == 0)
+                {
+                    plantList.Add(item);
+                    continue;
+                }
+                if (item <= plantList[plantList.Count - 1])
+                {
+                    plantList.Add(item);
+                }
+                else
+                {
+                    LinkList.AddLast(plantList);
+                    plantList = new List<int>();
+                    plantList.Add(item);
+                }
+            }
+            if (plantList.Count > 0)
+            {
+                LinkList.AddLast(plantList);
+            }
+            if (LinkList.Count == 1)
+            {
+                return days;
+            }
+            while (LinkList.Count > 1)
+            {
+                var current = LinkList.First;
+                while (current.Next != null)
+                {
+                    if (current.Next.Value.Count > 0)
+                    {
+                        current.Next.Value.RemoveAt(0);
+                    }
+
+                    if ((current.Next.Value.Count > 0 && current.Value.Last() > current.Next.Value.First()) || current.Next.Value.Count == 0)
+                    {
+                        if (current.Next.Value.Count > 0)
+                        {
+                            current.Value.AddRange(current.Next.Value);
+                        }
+                        LinkList.Remove(current.Next);
+                        continue;
+                    }
+                    current = current.Next;
+                }
+                days++;
+            }
+            return days;
+        }
     }
 }
