19867ba0f9f7445906e40cfb5cf985417afa3e64
KISLAY\Kislay <kislay_live@hotmail.com>
2025-09-15 20:48:00 +0800

    Added more code

---
+++ HackerRankCode/LongestValidParenthesesSolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCode
+{
+    internal class LongestValidParenthesesSolution
+    {
+        public int LongestValidParentheses(string s)
+        {
+            int maxLength = 0;
+            Stack<int> stack = new Stack<int>();
+            stack.Push(-1);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    stack.Pop();
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(i);
+                    }
+                    else
+                    {
+                        maxLength = Math.Max(maxLength, i - stack.Peek());
+                    }
+                }
+            }
+            return maxLength;
+        }
+    }
+}
---
+++ HackerRankCode/NextPermutationSolution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCode
+{
+    internal class NextPermutationSolution
+    {
+        public void NextPermutation(int[] nums)
+        {
+            int n = nums.Length;
+            int i = n - 2;
+            while (i >= 0 && nums[i] >= nums[i + 1])
+                i--;
+            if (i >= 0)
+            {
+                int j = i + 1;
+                while (nums[j] > nums[i] && j < n)
+                    j++;
+                Swap(nums, i, j - 1);
+            }
+            Reverse(nums, i + 1, n - 1);
+        }
+        private void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        private void Reverse(int[] nums, int start, int end)
+        {
+            while (start < end)
+            {
+                Swap(nums, start, end);
+                start++;
+                end--;
+            }
+        }
+    }
+}
